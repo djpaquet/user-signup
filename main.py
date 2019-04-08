83282eecee01cdd6094e95e3f0f5aefb5cfa6e4e
Dana Paquet <danapaquet@gmail.com>
2019-04-08 17:35:08 -0500

    Validation for username and password working

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ def index():
     return render_template('signup_form.html')
 
 @app.route("/", methods=["POST"])
-def username_validate():
+def user_validate():
     username = request.form['username']
     username_error = ''
-    char = ''
     if len(username) >= 3 and len(username) <= 20:
         for character in username:
             if character == ' ':
@@ -23,12 +22,32 @@ def username_validate():
         username_error = 'Username must be between 3-20 characters'
         username = ''
     
+    password = request.form['password']
+    password_error = ''
+    if len(password) >=3 and len(password) <=20:
+        for character in password:
+            if character == ' ':
+                password_error = 'Please enter a valid Password that has no spaces'
+                password = ''
+    elif len(password) < 3 or len(password) > 20:
+        password_error = 'Password must be between at least 6 characters'
+        password = ''
     
-    return render_template('signup_form.html', username=username, 
-                                username_error=username_error)
+    verify_password = request.form['verify_password']
+    verify_password_error = ''
+    if password != verify_password:
+        verify_password_error = 'Passwords do not match'
+        password = ''
+    
+    
+    return render_template('signup_form.html',username=username, 
+                                username_error=username_error,
+                                password=password, 
+                                password_error=password_error,
+                                verify_password=verify_password,
+                                verify_password_error=verify_password_error
+                                )
         
-@app.route('/', methods=['POST'])
-
 def email_validate():
     email = request.form['email']
     
